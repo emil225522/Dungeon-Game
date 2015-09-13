67176afbe1bc37366ac493ec85b1fd102ddd2585
emil225522 <ungothar@hotmail.com>
2015-09-13 21:31:47 +0200

    Flies will move to player

--- DungeonGame/DungeonGame/DungeonGame/Enemy/Fly.cs
+++ DungeonGame/DungeonGame/DungeonGame/Enemy/Fly.cs
@@ -18,7 +18,7 @@ namespace DungeonGame
             : base(position, new Animation(Content, "fly", 150, 2, true), seed, 1.5F, 15, 1)
         {
             direction = (Direction)values.GetValue(rnd.Next(values.Length));
-            circelingPlace = new Vector2(rnd.Next(50, 500), rnd.Next(50, 500));
+            circelingPlace = new Vector2(rnd.Next(50, 800), rnd.Next(50, 600));
             angleDirection = (float)rnd.Next(200, 500) / 10000;
             if (rnd.Next(2) == 1)
                 goingLeft = true;
@@ -27,6 +27,11 @@ namespace DungeonGame
         public override void Update(List<Tile> tiles, GameTime gameTime, Room room, Player player)
         {
             base.Update(tiles, gameTime, room, player);
+                float XDistance = position.X - player.position.X - 40;
+                float YDistance = position.Y - player.position.Y - 40;
+                //sets the velocity to that with the right angle thanks to this function
+                circelingPlace.X -= 1 * (float)Math.Cos(Math.Atan2(YDistance, XDistance));
+                circelingPlace.Y -= 1 * (float)Math.Sin(Math.Atan2(YDistance, XDistance));
             if (goingLeft)
                 angle -= angleDirection;
             else
