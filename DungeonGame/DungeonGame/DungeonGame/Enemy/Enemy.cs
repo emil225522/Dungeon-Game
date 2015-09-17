d36d093cf28da5c5eb2abb5e41c2b3fd8da0492a
Emil Jönsson <ungothar@hotmail.com>
2015-09-18 01:00:07 +0200

    ayy

--- DungeonGame/DungeonGame/DungeonGame/Enemy/Enemy.cs
+++ DungeonGame/DungeonGame/DungeonGame/Enemy/Enemy.cs
@@ -49,6 +49,7 @@ namespace DungeonGame
             this.speed = speed;
             this.hp = hp;
             this.type = type;
+
         }
 
         public override void Update(GameTime gameTime, Room room)
