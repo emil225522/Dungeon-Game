26cc6eafd9aee096ddd8ee660054fc49b03aabb3
emil225522 <ungothar@hotmail.com>
2016-04-12 21:49:15 +0200

    Slimes jump mechanic now fully functional

--- DungeonGame/DungeonGame/DungeonGame/Enemy/SlimeBoss.cs
+++ DungeonGame/DungeonGame/DungeonGame/Enemy/SlimeBoss.cs
@@ -13,6 +13,7 @@ namespace DungeonGame
         float angle;
         List<Vector2> points = new List<Vector2>();
         float angleDirection;
+        Texture2D shadowTexture;
         bool isSpawning;
         int timer;
         int index;
@@ -21,8 +22,10 @@ namespace DungeonGame
         float rotation;
         int jumptimer;
         float ypos;
+        Vector2 originalPosition;
         float yvel;
         bool isjumping;
+        bool isMassiveJumping;
         public override Rectangle HitBox { get { return new Rectangle((int)Position.X - Animation.frameWidth / 2, (int)Position.Y - Animation.frameHeight / 2, Animation.frameWidth, Animation.frameHeight); } }
 
         Color normalColor;
@@ -31,19 +34,23 @@ namespace DungeonGame
         public SlimeBoss(ContentManager Content, int seed, Vector2 position)
             : base(position, new Animation(Content, "slimeBoss", 150, 2, true), seed, 6, 400, 1, false, true)
         {
+            shadowTexture = Content.Load<Texture2D>("shadow");
             direction = (Direction)values.GetValue(rnd.Next(values.Length));
+            ypos = Position.Y;
+            originalPosition = Position;
         }
         public override void Update(GameTime gameTime, Room room)
         {
             base.Update(gameTime, room);
             jumptimer++;
-            if (!isjumping)
+            if (!isjumping && !isMassiveJumping)
             {
                 if (rnd.Next(100) == 5)
                 {
                     isjumping = true;
                     ypos = Position.Y;
-                    yvel = -7;
+                    Position = new Vector2(originalPosition.X + 30, Position.Y);
+                    yvel = -10;
                 }
                 else
                 {
@@ -58,51 +65,33 @@ namespace DungeonGame
             }
             if (isjumping)
             {
-                if (Position.Y < ypos)
+                if (Position.Y < originalPosition.Y)
                     yvel += 0.5f;
                 else
                     isjumping = false;
             }
-            if (isjumping == false)
+            if (isMassiveJumping)
             {
-
-                if (!IsColliding(room.tiles))
+                Vector2 directionTo = room.player.Position - new Vector2(originalPosition.X + 40,originalPosition.Y- 50);
+                directionTo.Normalize();
+                Position = new Vector2(originalPosition.X+ 30,Position.Y);
+                if (Position.Y > originalPosition.Y)
                 {
-                    if (direction == Direction.Down)
-                        Position += new Vector2(0, speed);
-                    else if (direction == Direction.Left)
-                        Position -= new Vector2(speed, 0);
-                    else if (direction == Direction.Right)
-                        Position += new Vector2(speed, 0);
-                    else if (direction == Direction.Up)
-                        Position -= new Vector2(0, speed);
+                    yvel = 0;
+                    isMassiveJumping = false;
                 }
                 else
-                {
-                    if (direction == Direction.Down)
-                    {
-                        Position -= new Vector2(0, speed * 4);
-                        direction = Direction.Up;
-                    }
-                    else if (direction == Direction.Left)
-                    {
-                        Position += new Vector2(speed * 4, 0);
-                        direction = Direction.Right;
-                    }
-                    else if (direction == Direction.Right)
-                    {
-                        Position -= new Vector2(speed * 4, 0);
-                        direction = Direction.Left;
-                    }
-                    else if (direction == Direction.Up)
-                    {
-                        Position += new Vector2(0, speed * 4);
-                        direction = Direction.Down;
-                    }
-                }
+                    yvel+= +0.5f;
+                originalPosition += directionTo * 2;
             }
+            //else if (!isjumping)
 
 
+        }
+        public override void OnHit()
+        {
+            yvel = -20;
+            isMassiveJumping = true;
         }
         public override void Draw(SpriteBatch spriteBatch)
         {
@@ -111,7 +100,7 @@ namespace DungeonGame
                 color = Color.Red;
             else
                 color = Color.White;
-
+            spriteBatch.Draw(shadowTexture,originalPosition,Color.White);
             animation.Draw(spriteBatch, new Vector2((int)Position.X, (int)Position.Y), color, rotation);
         }
     }
